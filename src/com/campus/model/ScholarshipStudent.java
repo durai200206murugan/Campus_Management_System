f7abfcc470d844cbf309064a612cd66f8bb1a0e2
durai200206murugan <duraimuruganmuthamil@gmail.com>
2026-09-24 15:33:51 +0530

    day4

--- src/com/campus/model/ScholarshipStudent.java
+++ src/com/campus/model/ScholarshipStudent.java
@@ -1,5 +1,24 @@
 package com.campus.model;
 
-public class ScholarshipStudent {
+public class ScholarshipStudent extends Student {
+    private double scholarshipPercentage;
+
+    public ScholarshipStudent(int studentid, String studentname, int age, String department, int[] marks, double scholarshipPercentage) {
+        super(studentid, studentname, age, department, marks);
+        this.scholarshipPercentage = scholarshipPercentage;
+    }
     
+    //getters and setters
+    public double getScholarshipPercentage() {
+        return scholarshipPercentage;
+    }
+
+    public void setScholarshipPercentage(double scholarshipPercentage) {
+        this.scholarshipPercentage = scholarshipPercentage;
+    }
+
+    @Override
+    public void StudentType() {
+        System.out.println("This is a Scholarship Student.");
+    }
 }
